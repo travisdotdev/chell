fde0ed365538391cc43ff255de3cfdd10389f8d8
travdotdev <108806867+travisdotdev@users.noreply.github.com>
2026-09-11 13:07:16 +0100

    Multi arg execution implemented

--- main.c
+++ main.c
@@ -17,22 +17,24 @@ int main(int argc, char *argv[]) {
 	// Trim newline
 	char *nl = strchr(buf, '\n');
 	if (nl) *nl = '\0';
+	
+	// Split buf into args
+	char *args[20];
+	int nargs = 0;
 
-	// buf[strcspn(buf, '\n')] = 0;
-	// printf("String: %s", buf);
-
+	args[nargs] = strtok(buf, " ");
+	while (args[nargs] != NULL)  {
+		args[++nargs] = strtok(NULL, " ");
+	}
 
 	// Fork and exec
 	pid_t pid = fork();
 
 	if (pid > 0) {
-		// Parent
 		wait(NULL);
 	}
 	else {
-		// Child
-		execlp(buf, buf, NULL);
-		// Something went wrong
+		execvp(args[0], args);
 		fprintf(stderr, "Could not exec %s\n", buf);
 	}
 }
